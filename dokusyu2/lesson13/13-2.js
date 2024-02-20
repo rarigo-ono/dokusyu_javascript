9f9128303676975b1edfd162785e71bc792ee6cd
nomizuono <ono@nomizuya.co.jp>
2024-02-20 12:59:37 +0900

    13.2.2の途中

--- dokusyu2/lesson13/13-2.js
+++ dokusyu2/lesson13/13-2.js
@@ -1,9 +1,26 @@
-setTimeout(() => {
-    console.log("A");
-}, 1000);
+let instance = new Promise(( resolve, reject) => {
 
-setTimeout(() => {
-    console.log("B");
+    //1秒後に実行
+    setTimeout(() => {
+        //0~10のランダムな値を取得
+        const rand = Math.floor(Math.random() * 11);
+
+        if( rand < 5 ) {
+            reject( rand ); //5未満はエラー
+        } else {
+            resolve( rand );    //5以上は成功
+        }
+    }, 1000);
 });
 
-console.log("C");
+instance = instance.then( value => {
+    console.log(`5以上の値[${ value }]が渡ってきました。`)
+});
+
+instance = instance.catch( errorValue => {
+    console.log(`5未満の値[${ value }]が渡ってきたためエラー表示。`)
+});
+
+instance = instance.finally( () => {
+    console.log(`処理を実行します。`)
+})
